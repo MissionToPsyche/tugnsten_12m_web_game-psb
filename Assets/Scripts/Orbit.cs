8292f0156e95ab51e11e0b40d764871ac42f0cd4
Garrett <96957645+eclipise@users.noreply.github.com>
2023-11-09 16:56:02 -0500

    refactored orbit and orbiter
    moved orbit logic into the orbit class, which the orbiter now uses

--- Assets/Scripts/Orbit.cs
+++ Assets/Scripts/Orbit.cs
@@ -14,4 +14,68 @@ public class Orbit : MonoBehaviour
     {
         lr = GetComponent<LineRenderer>();
     }
+
+    public void DrawOrbit(Vector3 position, Vector3 velocity)
+    {
+        const int maxSteps = 10000;
+        int usedSteps = maxSteps;
+
+        // This "virtual" orbiter will step through the real one's orbit without
+        // actually moving the real one. 
+        VirtualOrbiter virtualOrbiter = new(parent, velocity, position);
+
+        Vector3[] points = new Vector3[maxSteps];
+
+        // Step the orbit forward until either it completes a full orbit or
+        // reaches maxSteps many increments.
+        for (int step = 0; step < maxSteps; step++)
+        {
+            virtualOrbiter.UpdateVelocity();
+            virtualOrbiter.UpdatePosition();
+
+            points[step] = virtualOrbiter.position;
+
+            // Breaks the calculation early if the virtual orbiter has completed
+            // a full orbit, which is detected by checking if the first position
+            // calculated is very close to the current one.
+            if ((points[0] - points[step]).magnitude < .1 && step > 100)
+            {
+                usedSteps = step;
+                break;
+            }
+        }
+
+        lr.positionCount = usedSteps;
+        lr.SetPositions(points);
+    }
+
+    // This is essentially the Orbiter class but stripped down. We can't use the 
+    // actual Orbiter class because we can't make new instances of Unity monobehaviors.
+    private class VirtualOrbiter
+    {
+        private PointMass parent;
+        private Vector3 velocity;
+        public Vector3 position;
+
+        public VirtualOrbiter(PointMass p, Vector3 vel, Vector3 pos)
+        {
+            parent = p;
+            velocity = vel;
+            position = pos;
+        }
+
+        public void UpdateVelocity()
+        {
+            Vector3 dist = parent.transform.position - position;
+            float sqrDist = dist.sqrMagnitude;
+            Vector3 dir = dist.normalized;
+            Vector3 acceleration = PointMass.gravitationalConstant * parent.mass * dir / sqrDist;
+            velocity += PointMass.physicsTimeStep * acceleration;
+        }
+
+        public void UpdatePosition()
+        {
+            position += PointMass.physicsTimeStep * velocity;
+        }
+    }
 }
--- Assets/Scripts/Orbiter.cs
+++ Assets/Scripts/Orbiter.cs
@@ -3,22 +3,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(LineRenderer))]
 public class Orbiter : PointMass
 {
     public PointMass parent;
-    public LineRenderer lr;
+    public Orbit orbit;
     public Vector3 initialVelocity;
-    public Vector3 currentVelocity;
+    private Vector3 currentVelocity;
 
-    private const float thrustRate = 0.5f;
+    public const float thrustRate = 0.5f;
 
     public void Awake()
     {
         currentVelocity = initialVelocity;
         Time.fixedDeltaTime = physicsTimeStep;
-
-        lr = GetComponent<LineRenderer>();
+        orbit.parent = parent;
     }
 
     public void Update()
@@ -37,7 +35,7 @@ public class Orbiter : PointMass
     {
         UpdateVelocity();
         UpdatePosition();
-        DrawOrbit();
+        orbit.DrawOrbit(transform.position, currentVelocity);
     }
 
     private Vector3 GetNewAcceleration()
@@ -69,63 +67,5 @@ public class Orbiter : PointMass
     private void UpdatePosition()
     {
         transform.position += GetNewPosition();
-    }
-
-    void DrawOrbit()
-    {
-        const int maxSteps = 10000;
-        int usedSteps = maxSteps;
-
-        // Duplicates the orbiter
-        VirtualOrbiter virtualOrbiter = new(parent, currentVelocity, transform.position);
-
-        Vector3[] points = new Vector3[maxSteps];
-
-        // Step the orbit forward
-        for (int step = 0; step < maxSteps; step++)
-        {
-            virtualOrbiter.UpdateVelocity();
-            virtualOrbiter.UpdatePosition();
-
-            points[step] = virtualOrbiter.position;
-
-            if ((points[0] - points[step]).magnitude < .1 && step > 100)
-            {
-                Debug.Log("breaking");
-                usedSteps = step;
-                break;
-            }
-        }
-
-        lr.positionCount = usedSteps;
-        lr.SetPositions(points);
-    }
-
-    class VirtualOrbiter
-    {
-        private PointMass parent;
-        private Vector3 velocity;
-        public Vector3 position;
-
-        public VirtualOrbiter(PointMass p, Vector3 vel, Vector3 pos)
-        {
-            parent = p;
-            velocity = vel;
-            position = pos;
-        }
-
-        public void UpdateVelocity()
-        {
-            Vector3 dist = parent.transform.position - position;
-            float sqrDist = dist.sqrMagnitude;
-            Vector3 dir = dist.normalized;
-            Vector3 acceleration = gravitationalConstant * parent.mass * dir / sqrDist;
-            velocity += physicsTimeStep * acceleration;
-        }
-
-        public void UpdatePosition()
-        {
-            position += physicsTimeStep * velocity;
-        }
     }
 }

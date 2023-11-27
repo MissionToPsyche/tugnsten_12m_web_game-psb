c42583d5d877423f24506563634a5d1bf5e97f3b
Han Dang <77813972+Sori106@users.noreply.github.com>
2023-11-26 19:12:20 -0500

    SliceImage Drag functionality.

--- Assets/Scripts/SliceImage.cs
+++ Assets/Scripts/SliceImage.cs
@@ -12,6 +12,7 @@ public class SliceImage : MonoBehaviour
     private byte[] bytes;
     private List<Vector2> starts = new List<Vector2>();
     private List<GameObject> images = new List<GameObject>();
+    private ImageGameHelper imagerGameHelper;
 
 
     public void setOriginalImage(Texture2D tex)
@@ -48,10 +49,10 @@ public class SliceImage : MonoBehaviour
     }
 
 
-    public void slice() 
+    public void slice()
     {
         // can probably take out (just here to reset for testing)
-        foreach(GameObject obj in images)
+        foreach (GameObject obj in images)
         {
             Destroy(obj);
         }
@@ -73,46 +74,46 @@ public class SliceImage : MonoBehaviour
         float imgSize = 0.4f;
         // int numImgs = 7;
         // float imgSize = 0.4f;
-        float imgWidth = slicedWidth*imgSize;
-        float imgHeight = slicedHeight*imgSize;
+        float imgWidth = slicedWidth * imgSize;
+        float imgHeight = slicedHeight * imgSize;
 
         // overlap
         float minOverlap = 0.8f;
-        float yOverlap = imgHeight*minOverlap;
-        float xOverlap = imgWidth*minOverlap;
+        float yOverlap = imgHeight * minOverlap;
+        float xOverlap = imgWidth * minOverlap;
         Vector2 overlap = new Vector2(xOverlap, yOverlap);
 
         // difference
         float maxOverlap = 0.3f;
-        float yDiff = imgHeight*maxOverlap;
-        float xDiff = imgWidth*maxOverlap;
+        float yDiff = imgHeight * maxOverlap;
+        float xDiff = imgWidth * maxOverlap;
         Vector2 diff = new Vector2(xDiff, yDiff);
 
         // find a different img
         int tries = 10;
-        float spaceX = slicedWidth/tries;
-        float spaceY = slicedHeight/tries;
+        float spaceX = slicedWidth / tries;
+        float spaceY = slicedHeight / tries;
         float space = Mathf.Min(spaceX, spaceY);
         int gridSize = 6;
         List<int[]> cells = new List<int[]>();
-        for(int i = 0; i < Mathf.CeilToInt(gridSize*imgSize); i++)
+        for (int i = 0; i < Mathf.CeilToInt(gridSize * imgSize); i++)
         {
-            for(int j = 0; j < Mathf.CeilToInt(gridSize*imgSize); j++)
+            for (int j = 0; j < Mathf.CeilToInt(gridSize * imgSize); j++)
             {
-                cells.Add(new int[] {i, j});
+                cells.Add(new int[] { i, j });
             }
         }
 
 
         // creates images
-        for(int imgNum = 0; imgNum < numImgs; imgNum++) 
+        for (int imgNum = 0; imgNum < numImgs; imgNum++)
         {
             int ctr = 0;
             float imgXStart;
             float imgYStart;
             Vector2 start;
             int randomCell = Random.Range(0, cells.Count); // select a cell
-            
+
             // tries to find images different enough
             do
             {
@@ -126,8 +127,8 @@ public class SliceImage : MonoBehaviour
                 float cellHeight = slicedHeight / gridSize;
 
                 // get a start point in the cell
-                float adjustedCellWidth = cellWidth/space;
-                float adjustedCellHeight = cellHeight/space;
+                float adjustedCellWidth = cellWidth / space;
+                float adjustedCellHeight = cellHeight / space;
                 float randX = Random.Range(0, (int)adjustedCellWidth) * space;
                 float randY = Random.Range(0, (int)adjustedCellHeight) * space;
                 imgXStart = cellX * cellWidth + randX;
@@ -135,12 +136,12 @@ public class SliceImage : MonoBehaviour
                 start = new Vector2(imgXStart, imgYStart);
 
                 // TO DO: write code to use most different image
-                if(ctr > tries)
+                if (ctr > tries)
                 {
                     break;
                 }
 
-            }while(!isStartDifferent(start, diff) && !isOverlap(start, overlap));
+            } while (!isStartDifferent(start, diff) && !isOverlap(start, overlap));
 
             starts.Add(start);
             cells.RemoveAt(randomCell);
@@ -153,7 +154,7 @@ public class SliceImage : MonoBehaviour
 
     public bool isStartDifferent(Vector2 newStart, Vector2 diff)
     {
-        foreach(Vector2 start in starts)
+        foreach (Vector2 start in starts)
         {
             if (Mathf.Abs(start.x - newStart.x) < diff.x && Mathf.Abs(start.y - newStart.y) < diff.y)
             {
@@ -165,7 +166,7 @@ public class SliceImage : MonoBehaviour
 
     public bool isOverlap(Vector2 newStart, Vector2 overlap)
     {
-        foreach(Vector2 start in starts)
+        foreach (Vector2 start in starts)
         {
             if (Mathf.Abs(start.x - newStart.x) < overlap.x && Mathf.Abs(start.y - newStart.y) < overlap.y)
             {
@@ -182,14 +183,14 @@ public class SliceImage : MonoBehaviour
 
         float sectionSize = 0.5f;
         // TO DO: separate x vs y edgeAdjustment for left side ?? 
-        float edgeAdjustment = 1/3f; // left side
-        float sectionWidth = originalWidth*sectionSize;
-        float sectionHeight = originalHeight*sectionSize;
+        float edgeAdjustment = 1 / 3f; // left side
+        float sectionWidth = originalWidth * sectionSize;
+        float sectionHeight = originalHeight * sectionSize;
         // TO DO: multiple section width/height by a right edge adjustment ?? V
-        float sectionXStart = Random.Range(originalWidth*edgeAdjustment, originalWidth-sectionWidth);
-        float sectionYStart = Random.Range(originalHeight*edgeAdjustment, originalHeight-sectionHeight);
+        float sectionXStart = Random.Range(originalWidth * edgeAdjustment, originalWidth - sectionWidth);
+        float sectionYStart = Random.Range(originalHeight * edgeAdjustment, originalHeight - sectionHeight);
 
-        Rect regionToCopy = new Rect(sectionXStart, sectionYStart, sectionWidth, sectionHeight); 
+        Rect regionToCopy = new Rect(sectionXStart, sectionYStart, sectionWidth, sectionHeight);
         Texture2D slicedTexture = new Texture2D((int)regionToCopy.width, (int)regionToCopy.height); // Create a new Texture2D to store the sliced part
         // Get the pixels from the original texture within the specified region
         Color[] pixels = originalImage.GetPixels((int)regionToCopy.x, (int)regionToCopy.y, (int)regionToCopy.width, (int)regionToCopy.height);
@@ -206,17 +207,17 @@ public class SliceImage : MonoBehaviour
         int separation = 100;
 
         List<Vector2> initialPositions = new List<Vector2>();
-        for(int i = 0; i < 2; i ++)
+        for (int i = 0; i < 2; i++)
         {
-            for(int j = 0; j < 4; j++)
+            for (int j = 0; j < 4; j++)
             {
                 initialPositions.Add(new Vector2(300f + (displaySize + separation) * j, 900f - (displaySize + separation) * i));
             }
         }
-        
+
 
         GameObject imgObject = new GameObject();
-        imgObject.name = "img"+ imgNum;
+        imgObject.name = "img" + imgNum;
 
         RectTransform trans = imgObject.AddComponent<RectTransform>();
         trans.transform.SetParent(canvas.transform); // setting parent
@@ -228,9 +229,9 @@ public class SliceImage : MonoBehaviour
         trans.sizeDelta = new Vector2(displaySize, displaySize); // set the size of the image/gameobject
 
         // adding canvas group component
-         CanvasGroup group = imgObject.AddComponent<CanvasGroup>();
-         group.alpha = 1;
-         group.blocksRaycasts = true;
+        CanvasGroup group = imgObject.AddComponent<CanvasGroup>();
+        group.alpha = 1;
+        group.blocksRaycasts = true;
 
         // add script component
         // ScrptName script = imgObject.AddComponent<ScrptName>();
@@ -241,9 +242,14 @@ public class SliceImage : MonoBehaviour
         Vector2 imgSize = new Vector2(imgWidth, imgHeight);
         // sets the texture of the sprite to a section of the slicedImage and specifies the center of the new image
         image.sprite = Sprite.Create(slicedTexture, new Rect(start, imgSize), new Vector2(0.5f, 0.5f));
-        
+
         imgObject.transform.SetParent(canvas.transform); // sets the parent
-        
+
+        imgObject.AddComponent<Draggable>();
+        imgObject.AddComponent<SnapToTarget>(); //// need to take this snapToTarget and setTargetPosition in imageGameHelper
+
+        // Rect sliceRect = new Rect(start.x, start.y, imgWidth, imgHeight);
+        // imagerGameHelper.AddOriginalPosition(imgObject, sliceRect, originalImage);
 
         return imgObject;
     }
@@ -253,16 +259,16 @@ public class SliceImage : MonoBehaviour
     {
         path = "Assets/Psyche_Mission_RubinAsteroid_171203.png";
         bytes = File.ReadAllBytes(path);
-        originalImage = new Texture2D(1,1); // size will be replaced by image size
+        originalImage = new Texture2D(1, 1); // size will be replaced by image size
         originalImage.LoadImage(bytes); // create a texture2d asset from the image
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
 
-    
+
 }

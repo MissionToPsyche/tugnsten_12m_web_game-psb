54a6685d0f7957ebd4df72b7f44fdc0027b32764
Garrett <96957645+eclipise@users.noreply.github.com>
2024-03-27 09:00:47 -0400

    increased distortion intensity

--- Assets/Scripts/Gravity Science/DistortedOrbit.cs
+++ Assets/Scripts/Gravity Science/DistortedOrbit.cs
@@ -87,7 +87,7 @@ public class DistortedOrbit : MonoBehaviour
                 // Implements Gaussian function Ae^(-x^2/2c^2) where A is the
                 // intensity and c is the width. Intensity is reduced by an
                 // order of magnitude to make the distortions a reasonable size.
-                float y = distortion.intensity / 10 * Mathf.Exp(-Mathf.Pow(x, 2) / Mathf.Pow(2 * 10, 2));
+                float y = distortion.intensity / 8 * Mathf.Exp(-Mathf.Pow(x, 2) / Mathf.Pow(2 * 10, 2));
 
                 // The point being operated on in terms of the actual orbit.
                 int currentOrbitPoint = centerPoint + x;

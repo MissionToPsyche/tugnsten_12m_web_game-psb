ce5bdc30de57d52d0c0bd34ec17560d75bb2c47c
Han Dang <77813972+Sori106@users.noreply.github.com>
2024-04-14 20:03:56 -0400

    fix scorecard issue when merging.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 abstract public class GameController : MonoBehaviour
 {
     public GameTimer timer;
-    // public Scorecard scorecard;
+    public Scorecard scorecard;
     protected bool gameRunning = false;
     public int maxScore = 10000;
     public int score = -1;

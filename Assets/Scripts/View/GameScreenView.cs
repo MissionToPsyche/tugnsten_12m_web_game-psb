58930d3996bed1f734e03bbc5359d244b2eb2399
Han Dang <77813972+Sori106@users.noreply.github.com>
2024-04-15 20:05:52 -0400

    Revert "Merge branch 'main' branch into UI-design"
    This reverts commit ca781fc6b9f953c50c3dd88feb2ebb5c5a6f9ceb, reversing
    changes made to 2e813c20c976a1dbb0d5cda6e551a68816b918df.

--- Assets/Scripts/View/GameScreenView.cs
+++ Assets/Scripts/View/GameScreenView.cs
@@ -16,6 +16,7 @@ public class GameScreenUI : MonoBehaviour
     private void OnEnable()
     {
         Scene scene = SceneManager.GetActiveScene();
+        orbitGameController = new OrbitGameController();
         currentSceneName = scene.name;
         minigameIndex = titleController.getSceneIndex(currentSceneName);
 
@@ -78,7 +79,7 @@ public class GameScreenUI : MonoBehaviour
         closeBtn = infoPanel.Q<Button>("close-button");
     }
 
-    private void BindUIEvents()      
+    private void BindUIEvents()
     {
         optionsBtn.clicked += () => { optionsButtonClicked(); playSound(); };
         xBtn.clicked += () => { closePanel(); playSound(); };
@@ -100,14 +101,25 @@ public class GameScreenUI : MonoBehaviour
     }
 
     public void continueButtonClicked()
-    {
-        minigameIndex += 1;
-        // titleController.setMinigame(minigameIndex);
+    {   
+        // if (minigameTitle.text != "Orbit")
+        // {
+        //     int missionOrbit = orbitGameController.missionOrbit;
+        //     Debug.Log($"mision Orbit: {missionOrbit}");
+        //     addOrbitGameTransiton();
+            
+        // }
+        // else
+        // {
+            minigameIndex += 1;
+            if (minigameIndex < 4)
+            {
+                // Debug.Log($"Loading next minigame. Game index:  {minigameIndex}");
+                SceneManager.LoadScene(titleController.getSceneName(minigameIndex));
+                
+            }
+        // }
 
-        if (minigameIndex < 4)
-        {
-            SceneManager.LoadScene(titleController.getSceneName(minigameIndex));
-        }
     }
 
     public void infoClicked()
@@ -119,7 +131,8 @@ public class GameScreenUI : MonoBehaviour
     public void RegisterTabCallbacks()
     {
         UQueryBuilder<Label> tabs = GetAllTabs();
-        tabs.ForEach((Label tab) => {
+        tabs.ForEach((Label tab) =>
+        {
             tab.RegisterCallback<ClickEvent>(TabOnClick);
         });
     }
@@ -134,7 +147,7 @@ public class GameScreenUI : MonoBehaviour
             SelectTab(clickedTab);
         }
     }
-     private static bool TabIsCurrentlySelected(Label tab)
+    private static bool TabIsCurrentlySelected(Label tab)
     {
         return tab.ClassListContains("selectedTab");
     }
@@ -142,11 +155,11 @@ public class GameScreenUI : MonoBehaviour
     private void SelectTab(Label tab)
     {
         tab.AddToClassList("selectedTab");
-        if(tab.name == "Instructions")
+        if (tab.name == "Instructions")
         {
             showInfo();
         }
-        else if(tab.name == "science-context")
+        else if (tab.name == "science-context")
         {
             ShowContext();
         }
@@ -200,7 +213,7 @@ public class GameScreenUI : MonoBehaviour
         }
     }
 
-    
+
 
     public void closePanel()
     {
@@ -230,5 +243,11 @@ public class GameScreenUI : MonoBehaviour
     public VisualElement getTopContainer()
     {
         return gameTopContainer;
+    }
+
+    public void addOrbitGameTransiton()
+    {
+        orbitGameController.SetMissionOrbit(minigameIndex);
+        SceneManager.LoadScene("Orbit_subgame");
     }
 }

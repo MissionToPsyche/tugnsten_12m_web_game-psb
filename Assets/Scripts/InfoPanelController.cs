47264f4e4342964a232165392dd636cb5ce663ef
Han Dang <77813972+Sori106@users.noreply.github.com>
2024-04-17 17:14:35 -0400

    added Info panel controller methods.

--- Assets/Scripts/InfoPanelController.cs
+++ Assets/Scripts/InfoPanelController.cs
@@ -5,8 +5,9 @@ public class InfoPanelController : MonoBehaviour
 {
     public VisualElement root;
     public ScrollView infoScrollView;
+    public string minigameTitle;
 
-    public void ShowInfo(string minigameTitle)
+    public void ShowInfo()
     {
         string infoUxmlPath = $"UI/UXML/{minigameTitle}Info";
         VisualTreeAsset gameInfoTree = Resources.Load<VisualTreeAsset>(infoUxmlPath);
@@ -19,9 +20,40 @@ public class InfoPanelController : MonoBehaviour
         }
     }
 
+    public void ShowContext()
+    {
+        string infoUxmlPath = $"UI/UXML/{minigameTitle}Context";
+        VisualTreeAsset gameInfoTree = Resources.Load<VisualTreeAsset>(infoUxmlPath);
+
+
+        if (gameInfoTree != null)
+        {
+            infoScrollView.contentContainer.Clear();
+            VisualElement gameInfoContent = gameInfoTree.Instantiate();
+            infoScrollView.contentContainer.Add(gameInfoContent);
+        }
+        else
+        {
+            Debug.Log($"{minigameTitle}Context.uxml file not found.");
+        }
+    }
+
     public UQueryBuilder<Label> GetAllTabs()
     {
         return root.Query<Label>(className: "tab");
+    }
+
+        private void SelectTab(Label tab)
+    {
+        tab.AddToClassList("selectedTab");
+        if(tab.name == "Instructions")
+        {
+            ShowInfo();
+        }
+        else if(tab.name == "science-context")
+        {
+            ShowContext();
+        }
     }
 
     public void UnselectTab(Label tab)
